13a23149089774b183ee558ca0c8df455b073533
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-02-23 00:20:08 +0530

    till navbar

--- frontend/src/component/navbar-slider/navbar.jsx
+++ frontend/src/component/navbar-slider/navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import '../navbar-slider/navbar.css';
+// import IMG from "../../images/hero-bg.png";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="outer-nav">
+        <div className="nav">
+            <div className="logo">Health<sub>Care</sub></div>
+            <div className="nav-list">
+                <ul>
+                    <li><a href='#'>ABOUTUS</a></li>
+                    <li><a href='#'>DEPARTMENT</a></li>
+                    <li><a href='#'>DOCTORS</a></li>
+                    <li><a href='#'>CONTACT-US</a></li>
+                    <li><a href='#' className='login-button'>LOGIN/REGISTER</a></li>
+                </ul>
+            </div>
+        </div>
+        <div className="slider"></div>
+    </div>
+    
   )
 }
 
