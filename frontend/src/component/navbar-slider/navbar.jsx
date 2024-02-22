90cb7ff433387dc700c9e822b20fe752831ab1d2
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-02-23 01:33:21 +0530

    till navbar both section

--- frontend/src/component/navbar-slider/navbar.jsx
+++ frontend/src/component/navbar-slider/navbar.jsx
@@ -1,26 +1,62 @@
-import React from 'react'
-import '../navbar-slider/navbar.css';
+import React from "react";
+import "../navbar-slider/navbar.css";
 // import IMG from "../../images/hero-bg.png";
 
 function Navbar() {
   return (
     <div className="outer-nav">
-        <div className="nav">
-            <div className="logo">Health<sub>Care</sub></div>
-            <div className="nav-list">
-                <ul>
-                    <li><a href='#'>ABOUTUS</a></li>
-                    <li><a href='#'>DEPARTMENT</a></li>
-                    <li><a href='#'>DOCTORS</a></li>
-                    <li><a href='#'>CONTACT-US</a></li>
-                    <li><a href='#' className='login-button'>LOGIN/REGISTER</a></li>
-                </ul>
-            </div>
+      <div className="nav">
+        <div className="logo">
+          Health<sub>Care</sub>
         </div>
-        <div className="slider"></div>
+        <div className="nav-list">
+          <ul>
+            <li>
+              <a href="#">ABOUTUS</a>
+            </li>
+            <li>
+              <a href="#">DEPARTMENT</a>
+            </li>
+            <li>
+              <a href="#">DOCTORS</a>
+            </li>
+            <li>
+              <a href="#">CONTACT-US</a>
+            </li>
+            <li>
+              <a href="#" className="login-button">
+                LOGIN/REGISTER
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="slider">
+        <div className="slider-content">
+          <div className="first">
+            <div className="h1">
+              <h1>WE PROVIDE BEST</h1>
+              <h1>HEALTHCARE</h1>
+            </div>
+            <div className="para">
+              <p>
+                Explicabo esse amet tempora quibusdam laudantium, laborum eaque
+                magnam fugiat hic? Esse dicta aliquid error repudiandae earum
+                suscipit fugiat molestias, veniam, vel architecto veritatis
+                delectus repellat modi impedit sequi.
+              </p>
+
+              
+            </div>
+            <div >
+                <button className="slide-button">Read More</button></div>
+          </div>
+          <div className="second"></div>
+          <div className="third"></div>
+        </div>
+      </div>
     </div>
-    
-  )
+  );
 }
 
 export default Navbar;
