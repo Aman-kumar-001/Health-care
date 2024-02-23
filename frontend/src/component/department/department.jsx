c49003c9e42c81cf5db2f3cdaa804921d2bc4007
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-02-24 01:53:16 +0530

    done

--- frontend/src/component/department/department.jsx
+++ frontend/src/component/department/department.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "../department/department.css";
+import { FaHeartPulse } from "react-icons/fa6";
+import { FaFirstAid } from "react-icons/fa";
+import { GiHypodermicTest } from "react-icons/gi";
+import { FaHandHoldingMedical } from "react-icons/fa";
 
 function Department() {
   return (
@@ -12,12 +16,46 @@ function Department() {
         </p>
 
         <div className="option">
-            <div className="option1">
-              
+          <div className="option1">
+            <div className="d-logo">
+              <FaHeartPulse />
             </div>
-            <div className="option2">sdf</div>
-            <div className="option3">df</div>
-            <div className="option4">asdf</div>
+            <h2 className="dh2-opt">CARDIOLOGY</h2>
+            <p className="dp-opt">
+              fact that a reader will be distracted by the readable page when
+              looking at its layout.
+            </p>
+          </div>
+          <div className="option2">
+          <div className="d-logo">
+              <FaHandHoldingMedical />
+            </div>
+            <h2 className="dh2-opt">DIAGNOSIS</h2>
+            <p className="dp-opt">
+              fact that a reader will be distracted by the readable page when
+              looking at its layout.
+            </p>
+          </div>
+          <div className="option3">
+          <div className="d-logo">
+              < GiHypodermicTest />
+            </div>
+            <h2 className="dh2-opt">SURGERY</h2>
+            <p className="dp-opt">
+              fact that a reader will be distracted by the readable page when
+              looking at its layout.
+            </p>
+          </div>
+          <div className="option4">
+          <div className="d-logo">
+              <FaFirstAid />
+            </div>
+            <h2 className="dh2-opt">FIRST AID</h2>
+            <p className="dp-opt">
+              fact that a reader will be distracted by the readable page when
+              looking at its layout.
+            </p>
+          </div>
         </div>
       </div>
     </div>
