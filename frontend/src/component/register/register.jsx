d35a7faa66b0e79cd7e1bbe057651e62a792018b
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-02-26 01:10:22 +0530

    register page done

--- frontend/src/component/register/register.jsx
+++ frontend/src/component/register/register.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import '../register/register.css';
+import { IoIosMail } from "react-icons/io";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { CiLogin } from "react-icons/ci";
+import {  useNavigate } from "react-router-dom";
 
 function Register() {
+
+  const navigate = useNavigate();
+
   return (
-    <div>
-      sdfsd
+    <div className="register">
+      <div className="register-box">
+        <form>
+          <h1>LOGIN</h1>
+          <div>
+          <IoIosMail /> <input type='email' placeholder='EMAIL'/>
+          </div>
+          <div>
+         <RiLockPasswordFill /><input type='password' placeholder='PASSWORD' />
+          </div>
+          <div>
+            < CiLogin/><button className='login-button'>LOGIN</button>
+          </div>
+          <span>OR</span>
+          <div>
+            < CiLogin/><button className='login-button' onClick={() => navigate('/login')}>REGISTER</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
