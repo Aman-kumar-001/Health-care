eea2ce433b35e3117ff2d8ea2e01470f9396399b
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-02-28 14:32:45 +0530

    cart

--- frontend/src/component/cart/cart.jsx
+++ frontend/src/component/cart/cart.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import "../cart/cart.css";
+import {  useNavigate } from "react-router-dom";
 
 
 function Cart() {
+
+   const navigate = useNavigate();
   return (
-    <div>Cart</div>
+    <div className="cart">
+        <div className="shop-navbar">
+        <div className="shop-nav">
+          <div className="logo">
+            Health<sub>Care</sub>
+          </div>
+          <div className="nav-list">
+            <ul>
+              <li>
+                <a onClick={() => navigate("/")}>HOME</a>
+              </li>
+              <li>
+                <a onClick={() => navigate("/shop")}>SHOP</a>
+              </li>
+              <li>
+                <a href="#">DOCTORS</a>
+              </li>
+              <li>
+                <a href="#">CONTACT-US</a>
+              </li>
+              <li>
+                <a className="login-button" onClick={() => navigate("/login")}>
+                  LOGIN/REGISTER
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
