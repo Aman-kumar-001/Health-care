227ea87a933d87c59a9d265ea9c6cf55c2a5e6b5
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-03-01 02:16:43 +0530

    sdfs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,33 @@
-import {BrowserRouter  ,Routes ,Route} from "react-router-dom";
-import './App.css';
-import Home from './component/home/home';
-import Login from './component/login/login';
-import Register from './component/register/register';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./component/home/home";
+import Login from "./component/login/login";
+import Register from "./component/register/register";
 import Shop from "./component/shop/shop";
 import Cart from "./component/cart/cart";
 import { useState } from "react";
 
-
 function App() {
+  const [login, setlogin] = useState();
 
-  
-
-
-  const [login , setlogin] = useState();
-
-  const handledata = (data) =>{
+  const handledata = (data) => {
     setlogin(data);
-    
-  }
+  };
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-      
-        <Route path="/register" element={login ? <Home/> :<Register Ondata={handledata} />} ></Route>
-        <Route path="/shop" element={<Shop/>}></Route>
-        <Route path="/cart" element={<Cart/>}></Route>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+
+          <Route
+            path="/register"
+            element={login ? <Home /> : <Register Ondata={handledata} />}
+          ></Route>
+          <Route path="/shop" element={<Shop />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
         </Routes>
-     </BrowserRouter>
+      </BrowserRouter>
     </div>
   );
 }
