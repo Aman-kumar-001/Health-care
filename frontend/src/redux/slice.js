aeedf25833c71dd1cf22e135a14ea418908ab285
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-04-01 18:17:41 +0530

    redux cart features

---
+++ frontend/src/redux/slice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const cartSlice = createSlice({
+    name : 'cart',
+    initialState : [],
+    reducers : {
+        addItem(state ,  action ){
+            state.push(action.payload);
+        },
+        removeItem(state , action){
+            return state
+        },
+    },
+})
+
+export const { addItem , removeItem} = cartSlice.actions;
+export default cartSlice.reducer;
