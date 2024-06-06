f87d8088a3f28a4548e360a45e8f7eb660d8aa5c
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-06-06 20:17:06 +0530

    background fixed

--- frontend/src/component/payment/payment.jsx
+++ frontend/src/component/payment/payment.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import '../payment/payment.css';
 
 
 function payment() {
   return (
-    <div className="payment_conatiner">
-      payement
+    <div className="payment-form-container">
+      <form className="payment-form">
+        <h2>Payment Details</h2>
+        
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" required />
+
+        <label htmlFor="card-number">Card Number:</label>
+        <input type="text" id="card-number" name="card-number" required />
+
+        <label htmlFor="cvv">CVV:</label>
+        <input type="text" id="cvv" name="cvv" required />
+
+        <button type="submit">Submit Payment</button>
+      </form>
     </div>
     
 
